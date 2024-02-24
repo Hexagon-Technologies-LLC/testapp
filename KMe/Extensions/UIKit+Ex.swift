39c0e4d5d3a1086459ce7f372589008c4941fd5e
John Maheswaran <johnmahan7@gmail.com>
2024-02-24 17:32:08 -0500

    text processing helper extensions

---
+++ KMe/Extensions/UIKit+Ex.swift
@@ -0,0 +1,25 @@
+//
+//  UIKit+Ex.swift
+//  KMe
+//
+//
+
+import Foundation
+import UIKit
+
+public extension UIScreen {
+    static func visibleViewController(baseVC: UIViewController? = nil) -> UIViewController? {
+        let rootVC = baseVC ?? UIApplication.shared.windows.first(where: \.isKeyWindow)?.rootViewController
+        
+        if let nav = rootVC as? UINavigationController {
+            return visibleViewController(baseVC: nav.visibleViewController)
+        }
+        if let tab = rootVC as? UITabBarController, let selected = tab.selectedViewController {
+            return visibleViewController(baseVC: selected)
+        }
+        if let presented = rootVC?.presentedViewController {
+            return visibleViewController(baseVC: presented)
+        }
+        return rootVC
+    }
+}
